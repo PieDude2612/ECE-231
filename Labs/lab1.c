3b6b6e48f7d6e3238d71b47495457c5fe8ae0cb0
PieDude2612 <arjun.viswanathan2612@gmail.com>
2022-02-03 21:05:05 -0500

    Final changes to lab 1

--- Labs/lab1.c
+++ Labs/lab1.c
@@ -1,5 +1,5 @@
 // ECE 231, Lab 1
-// 2/2/22
+// 2/3/22
 // Arjun Viswanathan
 // Convert and print a user-input decimal number to its corresponding value in a user-specified base
 
@@ -29,6 +29,7 @@ void main() {
             scanf("%d", &inputs[1]);                    // as long as the base is not one of the allowed bases, it asks to repeat
         }
 
+        // Automatic conversion with built-in delimiters
         if(inputs[1] == 10) {
             printf("%d in base %d = %d \n", inputs[0], inputs[1], inputs[0]);
         } else if(inputs[1] == 2) {
@@ -39,6 +40,60 @@ void main() {
         } else if(inputs[1] == 16) {
             printf("%d in base %d = %X \n", inputs[0], inputs[1], inputs[0]);
         }
+
+        // Manual conversion with hardcode
+        // if(inputs[1] == 10) {
+        //     printf("%d in base %d = %d \n", inputs[0], inputs[1], inputs[0]);
+        // } else if(inputs[1] == 2) {
+        //     printf("%d in base %d = %s \n", inputs[0], inputs[1], binaryVal);
+        // } else if(inputs[1] == 8) {
+        //     char octVals[3];
+        //     int oct1 = 0; int oct2 = 0; int oct3 = 0;
+        //
+        //     for(int i = 0; i < 8; i++) {
+        //         if(i < 2) {
+        //             oct1 += (binaryVal[i]- '0') * pow(2, 1-i);
+        //         } else if(i >= 2 && i < 5) {
+        //             oct2 += (binaryVal[i]- '0') * pow(2, 4-i);
+        //         } else if(i >= 5) {
+        //             oct3 += (binaryVal[i]- '0') * pow(2, 7-i);
+        //         } // find decimal value equivalent for the numbers
+        //     }
+        //
+        //     octVals[0] = oct1 + '0';
+        //     octVals[1] = oct2 + '0';
+        //     octVals[2] = oct3 + '0';        // tack on ASCII 0 to offset into char
+        //     octVals[3] = '\0';
+        //
+        //     printf("%d in base %d = %s \n", inputs[0], inputs[1], octVals);
+        // } else if(inputs[1] == 16) {
+        //     char hexVals[2];
+        //     int hex1 = 0; int hex2 = 0;
+        //
+        //     for(int i = 0; i < 8; i++) {
+        //         if(i < 4) {
+        //             hex1 += (binaryVal[i]- '0') * pow(2, 4-i);
+        //         } else {
+        //             hex2 += (binaryVal[i]- '0') * pow(2, 7-i);
+        //         } // find decimal equivalent for numbers
+        //     }
+        //
+        //     if(hex1 < 10) {
+        //         hexVals[0] = hex1 + '0';    // tack on ASCII offset to get hex values
+        //     } else {
+        //         hexVals[0] = hex1 + '7';    // tack on ASCII offset to get hex values
+        //     }
+        //
+        //     if(hex2 < 10) {
+        //         hexVals[1] = hex2 + '0';    // tack on ASCII offset to get hex values
+        //     } else {
+        //         hexVals[1] = hex2 + '7';    // tack on ASCII offset to get hex values
+        //     }
+        //
+        //     hexVals[2] = '\0';
+        //
+        //     printf("%d in base %d = %s \n", inputs[0], inputs[1], hexVals);
+        // }
     }
 }
 
