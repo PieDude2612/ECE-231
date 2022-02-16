df229048dead43bd69d31426b12114468ae73f05
PieDude2612 <arjun.viswanathan2612@gmail.com>
2022-02-16 15:59:42 -0500

    Lab 2 setup. Need to incorporate switch inputs.

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -1,20 +1,52 @@
 // ECE 231, Lab 2
-// 2/14/22
+// 2/16/22
 // Arjun Viswanathan
-// Using the Arduino UNO board with VS Code 
+// LED blinking using switches. 
+// On switch 1 pushed, blink red LED at 5 Hz
+// On switch 2 pushed, blink green LED at 5 Hz
+// On switch 3 pushed, blink both LEDs at 5 Hz
+// On any two switches pushed, turn off all LEDs
+
+// |= : HIGH
+// &=~ : LOW
+// ^= : toggle
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define DELAY 1000
+#define DELAY 100
+#define SWITCH 10
+
+void blinkRED();
+void blinkGREEN();
 
 int main(void) {
-    DDRB = 1<<PB5;
+    DDRD |= (1<<5);                  // set output pins 5 and 6, leave others unaffected
+    DDRD |= (1<<6);
+    int sum = 0;
 
     while(1) {
-        PORTB = 1<<PB5;
-        _delay_ms(DELAY);
-        PORTB = ~ (1<<PB5);
-        _delay_ms(DELAY);
+        if(sum < (SWITCH / 2)) {
+            blinkRED();
+        } else if(sum > (SWITCH / 2) && sum < SWITCH) {
+            blinkGREEN();
+        } else if(sum > SWITCH) {
+            sum = 0;
+        }
+        sum += 1;
     }
     return 0;
+}
+
+void blinkRED() {
+    PORTD |= (1<<6);            // pin 6 HIGH
+    _delay_ms(DELAY);
+    PORTD &=~ (1<<6);           // pin 6 LOW
+    _delay_ms(DELAY);
+}
+
+void blinkGREEN() {
+    PORTD |= (1<<5);            // pin 5 HIGH
+    _delay_ms(DELAY);
+    PORTD &=~ (1<<5);           // pin 5 LOW
+    _delay_ms(DELAY);
 }
