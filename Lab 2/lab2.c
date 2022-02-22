7cf14e2fe7c0f03dbf1ee9579df2413c775a3303
PieDude2612 <arjun.viswanathan2612@gmail.com>
2022-02-22 15:49:06 -0500

    Updated lab 2 code to be more robust

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -1,5 +1,5 @@
 // ECE 231, Lab 2
-// 2/16/22
+// 2/22/22
 // Arjun Viswanathan
 // LED blinking using switches on Arduino UNO. 
 // On switch 1 pushed, blink red LED at 5 Hz
@@ -11,78 +11,43 @@
 // &=~ : LOW
 // ^= : toggle
 
+// Pinouts, Library Imports, Constants
 #include <avr/io.h>
 #include <util/delay.h>
-#define DELAY 100
 
-void blinkRED();
-void blinkGREEN();
-void blinkBOTH();
-void kill();
+#define DELAY 100                   // delay = 1000 (ms/s) / 10 (on-offs/s)
+#define GREENOUT 5                  // output pin 5 to blink GREEN LED
+#define REDOUT 6                    // output pin 6 to blink RED LED
+#define S1 2                        // input pin 2 from switch 1, blinks RED LED
+#define S2 3                        // input pin 3 from switch 2, blinks GREEN LED
+#define S3 4                        // input pin 4 from switch 3, blinks BOTH LEDs
 
 int main(void) {
-    DDRD = 0x00;                    // initialize input and output pins in D and C
-    DDRD |= (1<<5);                 // set output pins 5 and 6, leave others as inputs
-    DDRD |= (1<<6);
+    DDRD = 0x00;                    // initialize input pins
+    DDRD |= (1<<REDOUT);            // set output pins 5 and 6, leave others as inputs
+    DDRD |= (1<<GREENOUT);
 
-    int s1; int s2; int s3;
+    int switch1; int switch2; int switch3;
 
     while(1) {
-        s1 = PIND & (1<<2);         // switches 1, 2, and 3 sending input to pins 2, 3, 4
-        s2 = PIND & (1<<3);
-        s3 = PIND & (1<<4);
+        switch1 = PIND & (1<<S1);         // switches 1, 2, and 3 sending input to pins 2, 3, 4
+        switch2 = PIND & (1<<S2);
+        switch3 = PIND & (1<<S3);
 
-        if((s1 && s2) || (s1 && s3) || (s2 && s3)) {
-            kill();
-        } else if(s1){
-            blinkRED();
-        } else if(s2) {
-            blinkGREEN();
-        } else if(s3) {
-            blinkBOTH();
+        if(!((switch1 && switch2) || (switch1 && switch3) || (switch2 && switch3))) {
+            if(switch1){
+                PORTD |= (1<<REDOUT);               // pin 6 HIGH
+            } else if(switch2) {
+                PORTD |= (1<<GREENOUT);             // pin 5 HIGH
+            } else if(switch3) {
+                PORTD |= (1<<REDOUT);               // pin 6 HIGH
+                PORTD |= (1<<GREENOUT);             // pin 5 HIGH
+            }
         }
+        _delay_ms(DELAY);
+        PORTD &=~ (1<<REDOUT);             // pin 6 LOW
+        PORTD &=~ (1<<GREENOUT);           // pin 5 LOW
+        _delay_ms(DELAY);
     }
     return 0;
-}
-
-/* 
-Send periodic on-off signal to pin 6, which then blinks the red LED 
-on the Arduino board. 
-*/
-void blinkRED() {
-    PORTD |= (1<<6);            // pin 6 HIGH
-    _delay_ms(DELAY);
-    PORTD &=~ (1<<6);           // pin 6 LOW
-    _delay_ms(DELAY);
-}
-
-/*
-Send periodic on-off signal to pin 5, which then blinks the green LED
-on the Arduino board. 
-*/
-void blinkGREEN() {
-    PORTD |= (1<<5);            // pin 5 HIGH
-    _delay_ms(DELAY);
-    PORTD &=~ (1<<5);           // pin 5 LOW
-    _delay_ms(DELAY);
-}
-
-/*
-Send periodic on-off signal to both pins, causing both LEDs to blink
-*/
-void blinkBOTH(){
-    PORTD |= (1<<6);            // pin 6 HIGH
-    PORTD |= (1<<5);            // pin 5 HIGH
-    _delay_ms(DELAY);
-    PORTD &=~ (1<<6);           // pin 6 LOW
-    PORTD &=~ (1<<5);           // pin 5 LOW
-    _delay_ms(DELAY);
-}
-
-/*
-Stop blinking on all LEDs
-*/
-void kill(){
-    PORTD &=~ (1<<5);
-    PORTD &=~ (1<<6);
 }
