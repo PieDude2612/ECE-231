1c53caa3a000c024832f9e018e7b3385860662b6
PieDude2612 <arjun.viswanathan2612@gmail.com>
2022-04-12 14:25:14 -0400

    Added a fader to the RGB LED so it looks cool

--- Lab 4/lab4.c
+++ Lab 4/lab4.c
@@ -10,16 +10,16 @@
 #define RED 3
 #define GREEN 5
 #define BLUE 6
-#define DELAY 10
 
-void setIntensity(int intensity);
+void display(int intensity);
 
 int main() {
     DDRB = 1<<5;
-    DDRD = 1<<RED | 1<<GREEN | 1<<BLUE;
+    int brightness = 0;
 
     // Since 2 colors (pins 5 and 6) run on TCCR0A, we set COM0A1 and COM0B1 to high
     // Since 1 color (pin 3) runs on TCCR2A, we set COM0B1 to high
+    // Both run on clk/64 prescaler so CS01 and CS00 are high
     TCCR0A = 0xA3;
     TCCR0B = 0x03;
     TCCR2A = 0x23;
@@ -31,35 +31,68 @@ int main() {
         b1 = PIND & (1<<S1);
         b2 = PIND & (1<<S2);
 
+        // When no buttons are pressed, set DDRD 0 so no output is passed out
         if(b1) {
             // LED on dim brightness
-            setIntensity(10);
+            DDRD = 1<<RED | 1<<GREEN | 1<<BLUE;
+            brightness = 10;
         } 
         if(b2) {
             // LED on medium brightness
-            setIntensity(100);
+            DDRD = 1<<RED | 1<<GREEN | 1<<BLUE;
+            brightness = 50;
         } 
         if(b1 && b2) {
             // LED on max brightness
-            setIntensity(255);
+            DDRD = 1<<RED | 1<<GREEN | 1<<BLUE;
+            brightness = 255;
         }
         if(!b1 && !b2) {
             // LED off
-            PORTD = 0x00;
-            setIntensity(0);
+            DDRD = 0;
         }
+
+        display(brightness);
     }
 }
 
 /**
- * @brief Sets the upper bound on the timers, and controls the intensity of the LED
- * The timer counts upto the OCR and then shuts the signal off, resets the OCR and counts again
- * This will keep the LED at given intensity on RGB
- * As extra, we can loop the OCR values so the LED cycles different colors
+ * @brief Displays a fader of colors on the RGB LED. The intensity is set by the input variable which 
+ * control the brightness of the fader. * 
  * @param intensity 
  */
-void setIntensity(int intensity) {
-    OCR0A = intensity;
-    OCR0B = intensity;
-    OCR2B = intensity;
+void display(int intensity) {
+    int rgb_values[] = {intensity, 0, 0};         // order: red, green, blue
+    int fadeup = 1;                               // color to fade up
+    int fadedown = 0;                             // color to fade down
+
+    int count;
+
+    for(count = 0; count < 200; count++){
+        if(rgb_values[fadedown] < 0) {
+            rgb_values[fadedown] = 0;
+            fadedown += 1;
+
+            if(fadedown > 2) {
+                fadedown = 0;
+            }
+        }
+
+        if(rgb_values[fadeup] > intensity) {
+            rgb_values[fadeup] = intensity;
+            fadeup += 1;
+
+            if(fadeup > 2) { 
+                fadeup = 0;
+            }
+        }
+
+        OCR0A = rgb_values[2];
+        OCR0B = rgb_values[1];
+        OCR2B = rgb_values[0];
+
+        rgb_values[fadedown] -= 1;
+        rgb_values[fadeup] += 1;
+        _delay_ms(1);
+    }
 }
